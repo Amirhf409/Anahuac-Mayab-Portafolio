1d286ffff2ee7f64628f53ea24544e1830863938
Amir Hernández <amirhernandezfarah06@gmail.com>
2026-03-10 11:58:29 -0600

    Herencia

---
+++ S2P2_herencia/C#/Mamifero/Mamifero.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Mamifero{
+    private string nombre;
+    protected double peso;
+
+    public Mamifero(string n, double p)
+    {
+        this.nombre = n;
+        this.peso = p;
+    }
+
+    public string Nombre
+    {
+        get { return this.nombre; }
+        set { this.nombre = value; }
+    }
+
+    public override string ToString()
+    {
+        return "Mamifero\n [" + nombre + "," + peso + " kg ]";
+    }
+}
+
+//clase gato : herencia
+class Gato : Mamifero 
+{
+    private int nBigotes;
+//base es para representar la herencia o lo que se hereda, en java es super
+    public Gato(string n, double p, int b) : base(n, p)
+    {
+        this.nBigotes = b;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString()+"[Bigotes:" + nBigotes + "]";
+    }
+}
+
+class Vaca : Mamifero
+{
+    private double litrosDeLeche;
+
+    public Vaca(String n, double p, double l) : base(n, p)
+    {
+        this.litrosDeLeche = l;
+    }
+
+    public void calcularCantidadComida()
+    {
+        Console.WriteLine(this.peso * 0.03 + " kg de alimento");
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + "[Litros de leche:" + litrosDeLeche + "]";
+    }
+}
+
+class Ballena : Mamifero{
+
+    public Ballena(string n, double p) : base(n, p)
+    {
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+
---
+++ S2P2_herencia/C#/Mamifero/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class testMamifero
+{
+
+    public static void Main()
+    {
+        Gato mitch = new Gato("Mitch", 6.7, 10);
+        Vaca nani = new Vaca("Nani", 350, 20);
+        Ballena titi = new Ballena("Titi", 20000);
+
+        Console.WriteLine(mitch);
+        Console.WriteLine("");
+
+        Console.WriteLine(nani);
+        nani.calcularCantidadComida();
+        Console.WriteLine("");
+
+        Console.WriteLine(titi);
+        Console.WriteLine("");
+    }
+}

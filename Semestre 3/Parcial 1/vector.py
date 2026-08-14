47e42a1c0b7ece077e6fdd8e7dbe7b7db7c7f6d7
Amir Hernández <amirhernandezfarah06@gmail.com>
2026-08-14 12:58:31 -0600

    Primer avance

---
+++ Semestre 3/Parcial 1/vector.py
@@ -0,0 +1,10 @@
+class Vector():
+    def seleccion_numero():
+        vector = []
+        for i in range(10):
+            numero = int(input("Ingresa un numero entero: "))
+            vector.append(numero)
+
+        print(vector)  
+
+
